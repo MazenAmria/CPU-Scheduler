0a1ef94b64181ff22ce06d6dbb50952edde2d2b7
MazenAmria <t.mazenamria@gmail.com>
2020-10-28 14:44:37 +0200

    "Initial Commit..."

---
+++ src/Driver.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Driver {
+    public static void main(String[] args){
+        ArrayList<Process> processes = new ArrayList<>();
+        processes.add(
+                new Process(2, 1, 6, 0, 0, 0)
+        );
+        processes.add(
+                new Process(1, 2, 10, 0, 0, 0)
+        );
+        processes.add(
+                new Process(4, 7, 8, 0, 0, 0)
+        );
+        /*processes.add(
+                new Process(103, 4, 8, 0, 0, 0)
+        );
+        processes.add(
+                new Process(104, 3, 3, 0, 0, 0)
+        );*/
+        processes.add(
+                new Process(3, 5, 2, 0, 0, 0)
+        );
+        Scheduler scheduler1 = new ShortestJobFirstScheduler(processes);
+        Scheduler scheduler2 = new NonPreemptiveExplicitPriorityScheduler(processes, 10);
+        Scheduler scheduler3 = new PreemptiveExplicitPriorityScheduler(processes, 10);
+        Thread thread1 = new Thread(scheduler1);
+        Thread thread2 = new Thread(scheduler2);
+        Thread thread3 = new Thread(scheduler3);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        Scheduler scheduler4 = new ShortestJobFirstScheduler(processes);
+        Scheduler scheduler5 = new NonPreemptiveExplicitPriorityScheduler(processes, 10);
+        Scheduler scheduler6 = new PreemptiveExplicitPriorityScheduler(processes, 10);
+        scheduler4.run();
+        scheduler5.run();
+        scheduler6.run();
+        Scheduler scheduler7 = new MultiprogrammedWithUniformIOPercentage(processes, 80);
+        scheduler7.run();
+        for (Quantum quantum : scheduler7.getCpuLog()) {
+            System.out.println(quantum);
+        }
+    }
+}
